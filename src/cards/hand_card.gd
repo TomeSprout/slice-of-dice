07a1a229bcd5ba9b14fcee113b05ca68a2c42c73
Timothy <Rorie.TimothyJr@gmail.com>
2022-07-16 17:16:08 -0400

    Update hand_card.gd
    Create onready var for node selectors.
    Preload card handling sound.
    Add test func for playing sound when hovering cards.

--- src/cards/hand_card.gd
+++ src/cards/hand_card.gd
@@ -1,19 +1,28 @@
 extends Node2D
 
 
+onready var anim_player: = $AnimationPlayer as AnimationPlayer
+onready var snd_HandlePlayingCard: = $HandlePlayingCard as AudioStreamPlayer2D
+
 var num: int = 0
-var anim_player: = $AnimationPlayer as AnimationPlayer
+var Sound = preload('res://src/sounds/playing_card_single_place_down_on_table.mp3')
 
 
 func _ready() -> void:
 	pass
 
 
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	play_sounds()
+#	snd_HandlePlayingCard.play(0.0)
 
 
 func _on_mouse_entered() -> void:
-	num += 1
-	if num > 0:
-		anim_player.play('CardFloat')
+	anim_player.play('CardFloat')
+
+
+func play_sounds() -> void:
+	if (_on_mouse_entered()):
+		if !snd_HandlePlayingCard.is_playing():
+			snd_HandlePlayingCard.stream = Sound
+			snd_HandlePlayingCard.play()
