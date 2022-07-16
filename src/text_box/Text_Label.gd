64735709bcc7cbdf6e0aed3193dcc9331cac7116
Timothy <Rorie.TimothyJr@gmail.com>
2022-07-16 09:41:40 -0400

    Add text-display, rename deck dir

---
+++ src/text_box/Text_Label.gd
@@ -0,0 +1,40 @@
+# Code Order
+#
+# tool
+# class_name
+# extends
+# docstring
+#
+# signals
+# enums
+# constants
+# exported variables
+# private variables
+# onready variables
+#
+# optional built-in virtual _init method
+# built-in virtual _ready method
+# remaining built-in virtual methods
+# public methods
+# private methods
+
+
+extends Label
+
+var drawTextSpeed: int = 0
+var drawTextLimit: int = 1000
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	show_text_scroll()
+
+
+func show_text_scroll() -> void:
+	if drawTextSpeed < drawTextLimit:
+		drawTextSpeed += 1
+		self.visible_characters = drawTextSpeed
+#		yield(get_tree().create_timer(1000.0), "timeout")
+
